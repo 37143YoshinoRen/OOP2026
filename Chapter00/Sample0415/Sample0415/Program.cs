2baee40922030ba771dcf2642c6d5bcee52280bf
infosys <infosys@C4241>
2026-04-16 10:56:17 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter00/Sample0415/Sample0415/Program.cs
@@ -0,0 +1,42 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace Sample0415
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //入力
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("array[" + i + "] = ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            ////集計
+            //int sum = 0;
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    sum = sum + array[i];
+            //}
+            //出力
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine();
+                Console.Write("array[" + i + "]:" + array[i] + ":");
+                astOut(array[i]);
+            }
+            //合計
+            Console.WriteLine();
+            Console.WriteLine("合計" + array.Where(n => n % 2 == 0).Sum());
+
+        static void astOut(int num){
+                //＊出力
+                for (int j = 0; j < num; j++)
+                {
+                    Console.Write("*");
+                }
+            }
+        }
+    }
+}
