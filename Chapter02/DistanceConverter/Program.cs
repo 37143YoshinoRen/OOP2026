bc834718120f275479aefddbf3b6f613b6ae5c0f
吉野　漣 <oibc37143@ojs.ac.jp>
2026-04-22 09:33:14 +0900

    コマンドライン引数の対応

---
+++ Chapter02/DistanceConverter/Program.cs
@@ -0,0 +1,44 @@
+﻿double feet = 0;
+
+if (args.Length < 1 || args[0] == "-tom")
+{
+    PrintMeterToFeetList(1, 10); //メートルへ変換
+}
+else if (args.Length < 1 || args[0] == "-tof")
+{
+    PrintFeetToMeterList(1, 10); //フィートへ変換
+}
+else
+{
+    Console.WriteLine("引数エラー");
+}
+
+void PrintMeterToFeetList(int start, int end)
+{
+    //フィートからメートルへの対応表を出力
+    for (int meter = start; meter <= end; meter++)
+    {
+        double feet = MeterToFeet(meter);
+        Console.WriteLine($"{meter}m = {feet:0.0000}ft");
+    }
+}
+void PrintFeetToMeterList(int start, int end)
+{
+    //フィートからメートルへの対応表を出力
+    for (int feet = start; feet <= end; feet++)
+    {
+        double meter = FeetToMeter(feet);
+        Console.WriteLine($"{feet}ft = {meter:0.0000}m");
+    }
+}
+//フィートからメートルを求める
+double FeetToMeter(int fett)
+{
+    //feet * 0.3048
+    return feet * 0.3048;
+}
+//メートルからフィートを求める
+double MeterToFeet(int meter)
+{
+    return meter / 0.3048;
+}
