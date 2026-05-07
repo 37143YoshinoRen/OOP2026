fc1417eba6474a4b6680fafb09a39fdcb89d3564
吉野　漣 <oibc37143@ojs.ac.jp>
2026-05-07 11:19:19 +0900

    SalesCounterクラスの変更

--- Chapter02/売上集計プログラム/SalesCalculator/SalesCounter.cs
+++ Chapter02/売上集計プログラム/SalesCalculator/SalesCounter.cs
@@ -11,22 +11,34 @@ namespace SalesCalculator {
 
 
         //コンストラクタ
-        public SalesCounter(List<Sale> sales) {
-            sales = sales;
-            this.sales = sales;
+        public SalesCounter(string filePath) {
+            sales = ReadSales(filePath);
         }
 
+
+        public List<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>();  //リスト
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');　//カンマ区切りで分割
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2]),
+                };
+                sales.Add(sale);
+            }
+            return sales;
+        }
         //店舗別売り上げを求める
-        public Dictionary<string,int> GetPerStoreSales() {
+        public Dictionary<string, int> GetPerStoreSales() {
             Dictionary<string, int> dict = new Dictionary<string, int>();
 
             foreach (var sale in sales) {
-                if (dict.ContainsKey(sale.ShopName))  //すでに店舗名が辞書のキーに登録されているか？
-                    //登録されている場合
-                    dict[sale.ShopName] += sale.Amount; //売上を他仕込み
-                }eles
-                    //未登録の場合
-                    dict[sale.ShopName] = sale.Amount; //新規に売上を登録
+                if (dict.ContainsKey(sale.ShopName))
+                    dict[sale.ShopName] += sale.Amount;
+                else
+                    dict[sale.ShopName] = sale.Amount;
             }
             return dict;
         }
