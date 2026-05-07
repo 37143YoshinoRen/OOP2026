1938185bd5472c30c28598aad371c5575459f2bb
吉野　漣 <oibc37143@ojs.ac.jp>
2026-05-07 10:16:41 +0900

    初版完成

---
+++ Chapter02/売上集計プログラム/SalesCalculator/SalesCounter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCalculator {
+    //売り上げ集計クラス
+    public class SalesCounter {
+        private readonly List<Sale> sales;
+
+
+        //コンストラクタ
+        public SalesCounter(List<Sale> sales) {
+            sales = sales;
+            this.sales = sales;
+        }
+
+        //店舗別売り上げを求める
+        public Dictionary<string,int> GetPerStoreSales() {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            foreach (var sale in sales) {
+                if (dict.ContainsKey(sale.ShopName))  //すでに店舗名が辞書のキーに登録されているか？
+                    //登録されている場合
+                    dict[sale.ShopName] += sale.Amount; //売上を他仕込み
+                }eles
+                    //未登録の場合
+                    dict[sale.ShopName] = sale.Amount; //新規に売上を登録
+            }
+            return dict;
+        }
+
+    }
+}
