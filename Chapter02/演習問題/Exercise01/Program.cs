8b4a5b0feb5d476c0d6880cc5b6109acebcac26e
吉野　漣 <oibc37143@ojs.ac.jp>
2026-05-13 09:50:11 +0900

    バッファリング

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -2,6 +2,24 @@
     internal class Program {
         static void Main(string[] args) {
 
+            var songs = new List<Song>();
+
+            //"アーティスト名："出力
+            Console.Write("曲名");
+            string? title = Console.ReadLine();
+
+            Console.Write("アーティスト名");
+            string? srtistname = Console.ReadLine();
+
+            Console.Write("演奏時間（秒）：");
+            int length = Console.Read();
+
+            Console.ReadLine(); //バッファリング
+
+            Song song = new Song(title, srtistname, length);
+
+            songs.Add(song);
+
             var songs = new Song[] {
             new Song("Let it be", "The Beatles", 243),
              new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
@@ -14,7 +32,7 @@
 
         private static void PrintSongs(Song[] songs) {
             foreach (var song in songs) {
-            Console.WriteLine($"{song.Title}{song.ArtistName}");
+                Console.WriteLine($"{song.Title},{song.ArtistName},{song.Length / 60}:{song.Length % 60}:00");
             }
         }
     }
