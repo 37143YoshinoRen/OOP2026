7574b3b72a062bb797ab73289b0be631a2388aa8
吉野　漣 <oibc37143@ojs.ac.jp>
2026-05-13 10:18:08 +0900

    List<Song>

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -20,17 +20,11 @@
 
             songs.Add(song);
 
-            var songs = new Song[] {
-            new Song("Let it be", "The Beatles", 243),
-             new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
-             new Song("Close To You", "Carpenters", 276),
-             new Song("Honesty", "Billy Joel", 231),
-             new Song("I Will Always Love You", "Whitney Houston", 273),
-            };
+            
             PrintSongs(songs);
         }
 
-        private static void PrintSongs(Song[] songs) {
+        private static void PrintSongs(List<Song> songs) {
             foreach (var song in songs) {
                 Console.WriteLine($"{song.Title},{song.ArtistName},{song.Length / 60}:{song.Length % 60}:00");
             }
---
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            PrintFeetToMeterList(1, 10);
+            
+        }
+
+        //インチからメートルへの対応表を出力
+        private static void PrintFeetToMeterList(int v1,int v2) {
+
+        }
+    }
+}
