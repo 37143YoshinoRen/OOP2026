d2c2a853a146e1c0c51d91a2c82369d9292b1121
吉野　漣 <oibc37143@ojs.ac.jp>
2026-05-20 10:34:59 +0900

    P78まで

--- Chapter03/ラムダ式とLINQ/Section01/Program.cs
+++ Chapter03/ラムダ式とLINQ/Section01/Program.cs
@@ -2,21 +2,22 @@
     internal class Program {
 
         static void Main(string[] args) {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
-
-
-            var count = Count(numbers, n => n % 4 == 0 || n % 5 == 0 );
-            Console.WriteLine(count);
-        }
-
-        static int Count(int[] numbers,Predicate<int> judge) {
-            var count = 0;
-            foreach (var n in numbers) {
-                if (judge(n) == true) {
-                    count++;
-                }
+            var cities = new List<string> {
+                "Tokyo",
+                "New Delhi",
+                "Bangkok",
+                "London",
+                "Paris",
+                "Berlin",
+                "Canberra",
+                "Hong Kong",
+            };
+            //文字数が6文字以上・oを含む・最後がn
+            var exists = cities.Exists(s => 6 <= s.Length && s.Contains('o') && s.EndsWith('n'));
+            Console.WriteLine(exists);
+            
+            
+            
             }
-            return count;
         }
     }
-}
