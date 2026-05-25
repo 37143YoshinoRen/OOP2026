15d07942a040080a9d7d66f546c84d91a7def335
吉野　漣 <oibc37143@ojs.ac.jp>
2026-05-25 11:16:25 +0900

    2つの要素を入れ替える

--- Chapter04/nullに関するイディオム/Section04/Program.cs
+++ Chapter04/nullに関するイディオム/Section04/Program.cs
@@ -44,7 +44,19 @@ namespace Section04 {
             Console.WriteLine("売上高：" + amount);
 
             #endregion
-        }   
+
+            #region ２つの要素を入れ替える
+            int a = 10;
+            int b = 20;
+            Console.Write("入れ替え前：");
+            Console.WriteLine("a = " + a + " b = " + b);
+
+            (b, a) = (100, 200); //入れ替え
+
+            Console.Write("入れ替え後：");
+            Console.WriteLine("a = " + a + "b = " + b);
+            #endregion
+        }
         private static string? DefaultMessage() {
             return "DefaultMassage";
         }
