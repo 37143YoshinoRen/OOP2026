191461cf7576bb146e149773cf137606bb72ce1c
吉野　漣 <oibc37143@ojs.ac.jp>
2026-05-25 10:48:56 +0900

    nullに関するイディオムサンプル

--- Chapter03/演習問題/Exercise02/Program.cs
+++ Chapter03/演習問題/Exercise02/Program.cs
@@ -48,11 +48,10 @@ namespace Exercise02 {
 
         private static void Exercise2_4(List<string> cities) {
             //できたらGitのコメント「問題3.2.4完成」
-            var selected = cities.Where(s => s.Contains('B')).ToArray();
-            var s = cities.Select(c => c.Length <= selected);
-            Console.WriteLine(s);
-        }
-	        
+            var obj = cities.Where(s => s.StartsWith('B')).Select(s => new { s, s.Length });
+            foreach(var data in obj) {
+                Console.WriteLine(data.s + ":" + data.Length + "文字");
+            }
         }
     }
 }
