6a9d9c776d3abe9dba1bf2f4e94cec6a5c5313c1
吉野　漣 <oibc37143@ojs.ac.jp>
2026-05-27 11:54:07 +0900

    4.1.3まで終了

---
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -0,0 +1,53 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            List<string> langs = [
+        "C#", "Java", "Ruby", "PHP", "Python", "TypeScript",
+                "JavaScript", "Swift", "Go",
+            ];
+
+            Exercise1(langs);
+            Console.WriteLine("---");
+            Exercise2(langs);
+            Console.WriteLine("---");
+            Exercise3(langs);
+        }
+
+        private static void Exercise1(List<string> langs) {
+
+            Console.WriteLine("\nforach文で出力");
+            foreach (var s in langs) {
+                if(s.Contains('S'))
+                Console.WriteLine(s);
+                  }
+            
+            Console.WriteLine("\nfor文で出力");
+            for(int i = 0; i<langs.Count; i++) {
+                if (langs[i].Contains('S'))
+                    Console.WriteLine(langs[i]);
+            }
+
+            Console.WriteLine("\nWhile");
+            int count = 0;
+            while (count < langs.Count) {
+                if (langs[count].Contains('S'))
+                    Console.WriteLine(langs[count]);
+                count++;
+            }
+        }
+       
+        private static void Exercise2(List<string> langs) {
+            Console.WriteLine("\n ---4.1.2---");
+            //LINQを使用する(Where)
+            var selected = langs.Where(s => s.Contains('S'));
+                Console.WriteLine(selected);
+        }
+
+        private static void Exercise3(List<string> langs) {
+            Console.WriteLine("\n---4.1.3---");
+            Console.WriteLine(langs.Find(s => s.Length == 10) ?? "unknown");
+            Console.WriteLine(langs);
+            }
+        }
+    }
