0ee2b60d972e52990411f5645532a1561c390eec
吉野　漣 <oibc37143@ojs.ac.jp>
2026-05-28 10:18:59 +0900

    4.10 4.11　完成

--- Chapter04/演習問題/P112問題4_2のプロジェクト/Exercise02/Exercise02/Program.cs
+++ Chapter04/演習問題/P112問題4_2のプロジェクト/Exercise02/Exercise02/Program.cs
@@ -13,29 +13,56 @@ namespace Exercise02 {
         private static void Exercise1() {
             //if-else文を使用(コード4.9)
             var line = Console.ReadLine();
-            int num = int.Parse(line);
-            if(0 > num) {
-                Console.WriteLine(num);
-            }else if(100 > num && num >= 0) {
-                Console.WriteLine(num * 2);
-
-                }else if(100 <=num && 500 > num) {
-                Console.WriteLine(num * 3);
-            }else if(500 >= num) {
-                Console.WriteLine(num);
+            if (int.TryParse(line, out var num)) {
+                if (0 < num) {
+                    Console.WriteLine(num);
+                } else if (100 > num) {
+                    Console.WriteLine(num * 2);
+                } else if (500 > num) {
+                    Console.WriteLine(num * 3);
+                } else {
+                    Console.WriteLine(num);
+                }
             }
         }
-        
+
 
         private static void Exercise2() {
             //switch文を使用(コード4.10)
-
+            var line = Console.ReadLine();
+            if (int.TryParse(line, out var num)) {
+                switch (num) {
+                    case < 0:
+                        Console.WriteLine(num);
+                        break;
+                    case < 100:
+                        Console.WriteLine(num * 2);
+                        break;
+                    case < 500:
+                        Console.WriteLine(num * 3);
+                        break;
+                    default:
+                        Console.WriteLine(num);
+                        break;
+                }
+            }
         }
 
         private static void Exercise3() {
             //switch式を使用(コード4.11)
+            var line = Console.ReadLine();
+            if (int.TryParse(line, out var num)) {
+                var outNum = num switch {
+                    < 0 => num,
+                    < 100 => num * 2,
+                    < 500 => num * 3,
+                    _  => num
 
-
+                };
+                Console.WriteLine(outNum);
+            } else {
+                Console.WriteLine("入力値に誤りがあります");
+            }
         }
     }
 }
