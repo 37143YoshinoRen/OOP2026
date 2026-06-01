c5cd58138482f5799cb23e53f6a51301a4b12519
吉野　漣 <oibc37143@ojs.ac.jp>
2026-06-01 11:16:05 +0900

    5.1.2まで

---
+++ Chapter05/プロパティに関するイディオム/Section01/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Immutable;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            //var obj = new PasswordPolicy("aaaaa", "bbbbb");
+            //var data = obj.Name;
+
+            var ms = new MySample();
+            //変更不可のオブジェクトなので、Add,TomobeAtは新たなインスタンスを返す
+            var newList = ms.MyList.Add(6).RemoveAt(0);
+            ms.MyList.ForEach(n => Console.WriteLine($"{n}"));
+            Console.WriteLine(); //改行
+
+            newList.ForEach(n => Console.WriteLine($"{n}"));
+            Console.WriteLine(); //改行
+            }
+        }
+    }
+
+    class MySample {
+        public ImmutableList<int>MyList { get; private set; }
+
+        public MySample() {
+            var list = new List<int>() { 1, 2, 3, 4, 5 };
+            MyList = list.ToImmutableList();
+        }
+    }
+
+    class PasswordPolicy {  
+        //プロパティの初期化
+        public int MinimumLength { get; set; } = 8;
+
+        //読み取り専用プロパティ
+        public string GivenName { get; init; } = null!;
+        public string FamilvName { get; init; } = null!;
+
+        //getアクセサーのみを定義した読み取り専用プロパティ
+        //public string Name {
+        //    get { return FamilvName + " " + GivenName; }
+        //}
+
+        public PasswordPolicy(string familyName, string givenName) {
+            FamilvName = familyName;
+            GivenName = givenName;
+        }
+    }
+    
---
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01{
+    //5.1.1
+    public class YearMonth{
+        public int Year { get; init; }
+        public int Month { get; init; }
+        public static int Is21Century { get; } = 2026;
+
+        public bool Is21Century => 2001 <= Year && Year <= 2100;
+         
+        public YearMonth AddOneMonth
+
+
+        //コンストラクタ
+        public YearMonth(int year, int month) {
+            Year = year;
+            Month = month;
+        }
+    }
+}
