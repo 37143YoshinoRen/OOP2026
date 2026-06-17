19dc9cc6f0104e3fa8da4eb9e855e1ead93d6d3a
吉野　漣 <oibc37143@ojs.ac.jp>
2026-06-17 10:35:44 +0900

    6.4 プログラム例

--- Chapter06/演習問題/Exercise04/Program.cs
+++ Chapter06/演習問題/Exercise04/Program.cs
@@ -4,14 +4,10 @@ namespace Exercise04 {
     internal class Program {
         static void Main(string[] args) {
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
-            var str = line.Split(',','=');
-            ToJapanese("Novelist");
-            ToJapanese("BestWork");
-            ToJapanese("Born");
-            var re = String.Join(str);
-
-
-
+            foreach (var str in line.Split(';')) {
+                var word = str.Split('=');
+                Console.WriteLine($"{ToJapanese(word[0])}:{word[1]}");
+            }
 
         }
         static string ToJapanese(string key) {
