3e14aaf59b3ee08158629ba2799e8015006ddaf3
吉野　漣 <oibc37143@ojs.ac.jp>
2026-06-22 11:41:55 +0900

    「設問１_提出」

--- TestProject/Test01_01/ScoreCounter.cs
+++ TestProject/Test01_01/ScoreCounter.cs
@@ -8,29 +8,30 @@
         }
 
         //メソッドの概要：
-        private static IEnumerable<Student> ReadScore(string filePath) {
-            var sales = new List<Student>();
+        public IEnumerable<Student> ReadScore(string filePath) {
+            var student = new List<Student>();
             var lines = File.ReadAllLines(filePath);
-            
-
-
-
-
-
-
-
-            return sales;
+            foreach (var line in lines) {
+                var scr = line.Split(',');　//カンマ区切りで分割
+                var std = new Student {
+                    Name = scr[0],
+                    Subject = scr[1],
+                    Score = int.Parse(scr[2]),
+                };
+                student.Add(std);
+            }
+            return student;
         }
 
         //メソッドの概要：
         public IDictionary<string, int> GetPerStudentScore() {
             var dict = new Dictionary<string, int>();
-
-
-
-
-
-
+            foreach (var sco in _score) {
+                if (dict.ContainsKey(sco.Name))
+                    dict[sco.Name] += sco.Score;
+                else
+                    dict[sco.Name] = sco.Score;
+            }
             return dict;
         }
     }
