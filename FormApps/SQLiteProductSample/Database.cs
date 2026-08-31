8e2515f726aeeaae118258fe8f602111ab1458e5
吉野　漣 <oibc37143@ojs.ac.jp>
2026-08-31 14:12:12 +0900

    Datebaseクラス作成

--- FormApps/SQLiteProductSample/Database.cs
+++ FormApps/SQLiteProductSample/Database.cs
@@ -4,34 +4,42 @@ namespace SQLiteProductSample;
 
 public static class Database
 {
-    private static readonly string DatabasePath =
+    //DBファイルの保存場所
+    private static readonly string DatabasePath = 
         Path.Combine(AppContext.BaseDirectory, "products.db");
 
+    //SQLiteへ接続するための接続文字列
     private static readonly string ConnectionString =
         $"Data Source={DatabasePath}";
-
+    
+    //DBファイルの保存場所を外部から確認するための読み取り専用プロパティ
     public static string FilePath => DatabasePath;
 
-    public static SqliteConnection GetConnection()
-    {
+    //新しいSQLiteConnectionを生成して返す
+    public static SqliteConnection GetConnection() {
         return new SqliteConnection(ConnectionString);
     }
 
-    public static void Initialize()
-    {
+    //DBの初期化処理
+    public static void Initialize() {
+        //接続オブジェクトを生成する。
         using var connection = GetConnection();
+
+        //DBを開く
         connection.Open();
 
         using var command = connection.CreateCommand();
-        command.CommandText =
-        """
-        CREATE TABLE IF NOT EXISTS Products (
-            Id    INTEGER PRIMARY KEY AUTOINCREMENT,
-            Name  TEXT NOT NULL,
-            Price INTEGER NOT NULL CHECK (Price >= 0)
-        );
-        """;
 
+        command.CommandText =
+            """
+            CREATE TABLE IF NOT EXISTS Products (
+                Id      INTEGER PRIMARY KEY AUTOINCREMENT,
+                Name    TEXT NOT NULL,
+                Price   INTEGER NOT NULL CHECK(Price >= 0)
+            );;
+            """;
+
+        //結果行を返さないSQLを実行する
         command.ExecuteNonQuery();
     }
 }
